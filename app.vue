3687fdf81d7dfe574e4bbe44bbbd839d7a4517b9
jesse <92591280+jessejes@users.noreply.github.com>
2024-07-08 18:43:19 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <ul>
+      <ListItem title="Test" subtitle="Subtitle Test"  />
+    </ul>
+    <p class="copyright">© 2024 Jesse Bakenhus</p>
+  </div>
+</template>
---
+++ components/ListItem.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+  subtitle: string
+  image?: string
+}>()
+</script>
+
+<template>
+    <li>
+      <h3>{{ title }}</h3>
+      <p>{{ subtitle }}</p>
+    </li>
+</template>
+  
